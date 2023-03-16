deacbefd81005a173906ece023518dfd45dd910b
GiuliaSetti <giulia.setti96@gmail.com>
2023-03-16 18:42:51 +0100

    vue: milestone 1 - added array and printed on page

--- js/script.js
+++ js/script.js
@@ -19,7 +19,187 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+        user: {
+            name: "Sofia",
+            avatar: "img/avatar_io.jpg",
+
+        }, 
+        contacts: [
+            {
+            name: "Michele",
+            visible: true,
+            avatar:"./img/avatar_1.jpg",
+            messages: [
+                {
+                    date: "16/03/2023",
+                    message: "Oe, come va?",
+                    status: "sent",
+                },
+                {
+                    date: "16/03/2023",
+                    message: "Tutto bene?",
+                    status: "sent",
+                },
+                {
+                    date: "16/03/2023",
+                    message: "Sisi, tutto bene! Come è andata la lezione?",
+                    status: "received",
+                },
+                {
+                    date: "16/03/2023",
+                    message: "E' andata bene! Sto venendo via ora!",
+                    status: "sent",
+                }
+               
+              ],
+              //fine messaggi
+            },
+            //fine contatto
+            {
+                name: "Fabio",
+                visible: true,
+                avatar:"./img/avatar_2.jpg",
+                messages: [
+                    {
+                        date: "14/03/2023",
+                        message: "Allora ci vediamo sabato?",
+                        status: "received",
+                    },
+                    {
+                        date: "14/03/2023",
+                        message: "Certo! Decidi tu dove vuoi andre però, sei tu il festeggiato!",
+                        status: "sent",
+                    },
+                    {
+                        date: "15/03/2023",
+                        message: "Pensavo di andare in Cambusa!",
+                        status: "received",
+                    },
+                    {
+                        date: "15/03/2023",
+                        message: "Perfetto!",
+                        status: "sent",
+                    }
+                   
+                  ],
+                  //fine messaggi
+            },
+            //fine contatto
+            {
+                name: "Samuele",
+                visible: true,
+                avatar:"./img/avatar_3.jpg",
+                messages: [
+                    {
+                        date: "12/03/2023",
+                        message: "Ti ho mandato tutto il materiale per mail!",
+                        status: "sent",
+                    },
+                    {
+                        date: "13/03/2023",
+                        message: "(Grazie per tutto, comunque)",
+                        status: "sent",
+                    },
+                    {
+                        date: "15/03/2023",
+                        message: "Grazie, ho ricevuto tutto!",
+                        status: "received",
+                    },
+                    {
+                        date: "15/03/2023",
+                        message: "Bene, perfetto! Domani ti mando il resto!",
+                        status: "sent",
+                    },
+                    {
+                        date: "16/03/2023",
+                        message: "Mandato anche il resto!",
+                        status: "sent",
+                    }
+                   
+                  ],
+                  //fine messaggi
+            },
+            //fine contatto
+            {
+                name: "Claudia",
+                visible: true,
+                avatar:"./img/avatar_6.jpg",
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Ciao Claudia, hai novità?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Non ancora',
+                        status: 'received'
+                    },
+                    {
+                        date: '10/01/2020 15:51:00',
+                        message: 'Nessuna nuova, buona nuova',
+                        status: 'sent'
+                    }
+        
+                   
+                  ],
+                  //fine messaggi
+            },
+            //fine contatto
+            {
+                name: 'Federico',
+                avatar: './img/avatar_7.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                        status: 'received'
+                    }
+                ],
+        
+                  //fine messaggi
+            },
+            //fine contatto
+            {
+                name: 'Davide',
+                avatar: './img/avatar_8.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                        status: 'received'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:51:00',
+                        message: 'OK!!',
+                        status: 'received'
+                    }
+                ],
+        
+                  //fine messaggi
+            },
+            //fine contatto
+      
+      
+          ],
+          // fine array contatti
+
+          counter: 0,
     }
-  }
+  },
+
+
+
 }).mount('#app')
