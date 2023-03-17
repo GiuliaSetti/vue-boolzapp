3fb54a72482684b9293e33c217ff73667ebdd1bf
GiuliaSetti <giulia.setti96@gmail.com>
2023-03-17 12:46:22 +0100

    fix: fixed style+vue: changeChat@click+time/access

--- js/script.js
+++ js/script.js
@@ -28,25 +28,30 @@ createApp({
             {
             name: "Michele",
             visible: true,
+            access: "Ultimo accesso alle 16:00",
             avatar:"./img/avatar_1.jpg",
             messages: [
                 {
                     date: "16/03/2023",
+                    time: "15:35",
                     message: "Oe, come va?",
                     status: "sent",
                 },
                 {
                     date: "16/03/2023",
+                    time: "15:35",
                     message: "Tutto bene?",
                     status: "sent",
                 },
                 {
                     date: "16/03/2023",
+                    time: "15:37",
                     message: "Sisi, tutto bene! Come è andata la lezione?",
                     status: "received",
                 },
                 {
                     date: "16/03/2023",
+                    time: "15:38",
                     message: "E' andata bene! Sto venendo via ora!",
                     status: "sent",
                 }
@@ -58,25 +63,30 @@ createApp({
             {
                 name: "Fabio",
                 visible: true,
+                access: "Ultimo accesso alle 15:46",
                 avatar:"./img/avatar_2.jpg",
                 messages: [
                     {
                         date: "14/03/2023",
+                        time: "10:10",
                         message: "Allora ci vediamo sabato?",
                         status: "received",
                     },
                     {
                         date: "14/03/2023",
+                        time: "12:25",
                         message: "Certo! Decidi tu dove vuoi andre però, sei tu il festeggiato!",
                         status: "sent",
                     },
                     {
                         date: "15/03/2023",
+                        time: "12:29",
                         message: "Pensavo di andare in Cambusa!",
                         status: "received",
                     },
                     {
                         date: "15/03/2023",
+                        time: "13:38",
                         message: "Perfetto!",
                         status: "sent",
                     }
@@ -88,30 +98,36 @@ createApp({
             {
                 name: "Samuele",
                 visible: true,
+                access: "Ultimo accesso alle 09:37",
                 avatar:"./img/avatar_3.jpg",
                 messages: [
                     {
                         date: "12/03/2023",
+                        time: "08:12",
                         message: "Ti ho mandato tutto il materiale per mail!",
                         status: "sent",
                     },
                     {
-                        date: "13/03/2023",
+                        date: "12/03/2023",
+                        time: "08:12",
                         message: "(Grazie per tutto, comunque)",
                         status: "sent",
                     },
                     {
-                        date: "15/03/2023",
+                        date: "13/03/2023",
+                        time: "08:30",
                         message: "Grazie, ho ricevuto tutto!",
                         status: "received",
                     },
                     {
-                        date: "15/03/2023",
+                        date: "13/03/2023",
+                        time: "09:00",
                         message: "Bene, perfetto! Domani ti mando il resto!",
                         status: "sent",
                     },
                     {
-                        date: "16/03/2023",
+                        date: "14/03/2023",
+                        time: "09:07",
                         message: "Mandato anche il resto!",
                         status: "sent",
                     }
@@ -123,20 +139,24 @@ createApp({
             {
                 name: "Claudia",
                 visible: true,
+                access: "Ultimo accesso alle 22:38",
                 avatar:"./img/avatar_6.jpg",
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '10/01/2020',
+                        time:" 15:30",
                         message: 'Ciao Claudia, hai novità?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '10/01/2020',
+                        time:" 15:50",
                         message: 'Non ancora',
                         status: 'received'
                     },
                     {
-                        date: '10/01/2020 15:51:00',
+                        date: '10/01/2020',
+                        time: "15:51",
                         message: 'Nessuna nuova, buona nuova',
                         status: 'sent'
                     }
@@ -150,14 +170,17 @@ createApp({
                 name: 'Federico',
                 avatar: './img/avatar_7.jpg',
                 visible: true,
+                access: "Ultimo accesso alle 23:12",
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '10/01/2020',
+                        time: "15:30",
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '10/01/2020',
+                        time: "15:50",
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
                         status: 'received'
                     }
@@ -169,20 +192,24 @@ createApp({
             {
                 name: 'Davide',
                 avatar: './img/avatar_8.jpg',
+                access: "Ultimo accesso alle 11:04",
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '10/01/2020',
+                        time: "15:30",
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
                         status: 'received'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '10time: "15:50",/01/2020',
+                        time: "15:50",
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:51:00',
+                        date: '10/01/2020',
+                        time: "15:510",
                         message: 'OK!!',
                         status: 'received'
                     }
@@ -199,6 +226,18 @@ createApp({
           counter: 0,
     }
   },
+
+  methods:{
+
+    changeChat(index){
+          
+        this.counter= index;
+
+    },
+
+
+
+  }
 
 
 
